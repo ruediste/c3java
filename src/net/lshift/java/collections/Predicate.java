6d485133bda2cbc7233d1d3a37294525ebfec980
david <david@localhost>
2005-08-16 15:48:06 +0100

    bug none: there is no way for me to easily use this component in a JDK 1.4 based project with these files included.

--- src/net/lshift/java/collections/Predicate.java
+++
@@ -1,8 +0,0 @@
-
-package net.lshift.java.collections;
-
-public interface Predicate<T>
-    extends Transform<T, Boolean>
-{
-
-}
--- src/net/lshift/java/collections/Transform.java
+++
@@ -1,7 +0,0 @@
-
-package net.lshift.java.collections;
-
-public interface Transform<T,U>
-{
-    public U apply(T x);
-}
