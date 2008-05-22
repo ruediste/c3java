15ac06930193f260b4a65c2b0c85b63f33871143
david <david@localhost>
2008-05-22 16:40:15 +0100

    bug none: more comprehensive test for primitive default values

--- test/src/net/lshift/beans/MockTest.java
+++ test/src/net/lshift/beans/MockTest.java
@@ -16,6 +16,20 @@ public class MockTest
         public void setInt1(int i);
         public String getString1();
         public void setString1(String s);
+        public boolean getBoolean1();
+        public void setBoolean1(boolean b);
+        public byte getByte1();
+        public void setByte1(byte b);
+        public short getShort1();
+        public void setShort1(short s);
+        public long getLong1();
+        public void setLong1(long l);
+        public char getChar1();
+        public void setChar1(char c);
+        public double getDouble1();
+        public void setDouble1(double d);
+        public float getFloat1();
+        public void setFloat1(float f);
     }
     
     interface ExampleSubclassBean
@@ -50,5 +64,18 @@ public class MockTest
         instance.setString1("foo");
         assertEquals("foo", instance.getString1());
         assertEquals("foo", store.get("string1"));
+    }
+    
+    public void testDefaultValues()
+    throws Exception
+    {
+        ExampleBean instance = Mock.bean(ExampleBean.class);
+        assertEquals(0, instance.getByte1());
+        assertEquals(0, instance.getChar1());
+        assertEquals(0, instance.getInt1());
+        assertEquals(0, instance.getLong1());
+        assertEquals(new Float(0.0), instance.getFloat1());
+        assertEquals(new Double(0.0), instance.getDouble1());
+        assertFalse(instance.getBoolean1());
     }
 }
